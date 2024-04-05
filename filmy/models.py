db26fc9434ad9f3f794ff33d6bc9bbda1b157217
Matej Kratochvil <matej.kratochvil@student.gyarab.cz>
2024-04-05 11:22:04 +0200

    5.4.

--- filmy/models.py
+++ filmy/models.py
@@ -6,3 +6,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text="in minutes")
+
+    def __str__(self):
+        return self.title
